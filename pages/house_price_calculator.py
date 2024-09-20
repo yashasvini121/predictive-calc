5bc79a36f0da4c0f31ad493c1c5776a2744be158
yashasvini <ysmarina1234@gmail.com>
2024-09-20 20:24:47 +0530

    adding plots

--- pages/house_price_calculator.py
+++ pages/house_price_calculator.py
@@ -1,34 +1,66 @@
 import streamlit as st
 from form_handler import FormHandler
-from models.house_price.predict import house_price_prediction_model
+from models.house_price.predict import house_price_prediction_model, model_details
 
 # Page Title
 st.title("House Price Prediction Calculator")
 
 # Defines the field mappings for the form
 field_mappings = {
-    "Area (in square feet)": "area",
-    "Near Main Road": "mainroad",
-    "Guest Room": "guestroom",
-    "Basement": "basement",
-    "Hot Water Heating": "hotwaterheating",
-    "Air Conditioning": "airconditioning",
-    "Preferred Area": "prefarea",
-    "Number of Bedrooms": "bedrooms",
-    "Number of Bathrooms": "bathrooms",
-    "Number of Stories": "stories",
-    "Parking Spaces": "parking",
-    "Furnishing Status": "furnishingstatus",
+	"Area (in square feet)": "area",
+	"Near Main Road": "mainroad",
+	"Guest Room": "guestroom",
+	"Basement": "basement",
+	"Hot Water Heating": "hotwaterheating",
+	"Air Conditioning": "airconditioning",
+	"Preferred Area": "prefarea",
+	"Number of Bedrooms": "bedrooms",
+	"Number of Bathrooms": "bathrooms",
+	"Number of Stories": "stories",
+	"Parking Spaces": "parking",
+	"Furnishing Status": "furnishingstatus",
 }
 
 # Creates the form with FormHandler, using the field mappings
 house_form = FormHandler(
-    name="House Price Estimator",
-    button_label="Predict Price",
-    model=house_price_prediction_model,
-    config_path="calculator_config/house_price.json",
-    field_mappings=field_mappings,
+	name="House Price Estimator",
+	button_label="Predict Price",
+	model=house_price_prediction_model,
+	config_path="calculator_config/house_price.json",
+	field_mappings=field_mappings,
 )
 
-# Render the form
-house_form.render()
+# Create tabs for the calculator and model details
+tabs = st.tabs(["Calculator", "Model Details"])
+
+# Renders the calculator in the first tab
+with tabs[0]:
+	house_form.render()
+
+
+metrics, prediction_plot, error_plot = model_details().evaluate()
+
+# Renders model details in the second tab
+with tabs[1]:
+	st.header("Model Details")
+	st.subheader(
+		f"Model Accuracy: {metrics['Test_R2']:.2%}"
+	)
+	st.subheader(
+		f"Scores: {metrics['Train_R2']:.2f}, {metrics['Test_R2']:.2f}"
+	)
+
+	# Displays scatter plot for actual vs predicted
+	st.subheader("Plots")
+	st.pyplot(prediction_plot)
+
+	# todo: fix this, both the error_plot and performance_plot are combined
+	
+	# Displays error terms plot
+	# st.subheader("Error Terms Distribution")
+	# st.pyplot(error_plot)
+
+	# Displays performance comparison graph
+	# st.subheader("Model Performance Graph")
+	# performance_plot = model_details().plot_performance_graph()
+	# st.pyplot(performance_plot)
