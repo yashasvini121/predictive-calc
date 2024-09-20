76fc30a77dc1d0280ace461ba6d362293939d927
yashasvini <ysmarina1234@gmail.com>
2024-09-20 19:27:02 +0530

    added model_evalution fxn to house_price_model

--- models/house_price/model.py
+++ models/house_price/model.py
@@ -3,10 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
-from statsmodels.formula import api
-from sklearn.metrics import mean_squared_error
 import warnings
 import pickle
+from ModelEvaluation import ModelEvaluation
 
 warnings.filterwarnings("ignore")
 
@@ -24,21 +23,21 @@ categorical_features = [col for col in features if nu[col] <= 16]
 
 # Removing outliers using IQR
 def remove_outliers(df, numerical_features):
-    for feature in numerical_features:
-        Q1 = df[feature].quantile(0.25)
-        Q3 = df[feature].quantile(0.75)
-        IQR = Q3 - Q1
-        df = df[(df[feature] >= (Q1 - 1.5 * IQR)) & (df[feature] <= (Q3 + 1.5 * IQR))]
-    return df.reset_index(drop=True)
+	for feature in numerical_features:
+		Q1 = df[feature].quantile(0.25)
+		Q3 = df[feature].quantile(0.75)
+		IQR = Q3 - Q1
+		df = df[(df[feature] >= (Q1 - 1.5 * IQR)) & (df[feature] <= (Q3 + 1.5 * IQR))]
+	return df.reset_index(drop=True)
 
 
 # Handling missing values
 def handle_missing_values(df):
-    null_summary = df.isnull().sum()
-    null_percentage = (null_summary / df.shape[0]) * 100
-    return pd.DataFrame(
-        {"Total Null Values": null_summary, "Percentage": null_percentage}
-    ).sort_values(by="Percentage", ascending=False)
+	null_summary = df.isnull().sum()
+	null_percentage = (null_summary / df.shape[0]) * 100
+	return pd.DataFrame(
+		{"Total Null Values": null_summary, "Percentage": null_percentage}
+	).sort_values(by="Percentage", ascending=False)
 
 
 # Removes outliers from numerical features
@@ -47,27 +46,27 @@ df = remove_outliers(df, numerical_features)
 # Filters categorical features without missing values
 null_value_summary = handle_missing_values(df)
 valid_categorical_features = [
-    col
-    for col in categorical_features
-    if col not in null_value_summary[null_value_summary["Percentage"] != 0].index
+	col
+	for col in categorical_features
+	if col not in null_value_summary[null_value_summary["Percentage"] != 0].index
 ]
 
 # Encoding categorical features
 def encode_categorical_features(df, categorical_features):
-    for feature in categorical_features:
-        # Binary encoding for features with 2 unique values
-        if df[feature].nunique() == 2:
-            df[feature] = pd.get_dummies(df[feature], drop_first=True, prefix=feature)
-        # Dummy encoding for features with more than 2 unique values
-        elif 2 < df[feature].nunique() <= 16:
-            df = pd.concat(
-                [
-                    df.drop([feature], axis=1),
-                    pd.get_dummies(df[feature], drop_first=True, prefix=feature),
-                ],
-                axis=1,
-            )
-    return df
+	for feature in categorical_features:
+		# Binary encoding for features with 2 unique values
+		if df[feature].nunique() == 2:
+			df[feature] = pd.get_dummies(df[feature], drop_first=True, prefix=feature)
+		# Dummy encoding for features with more than 2 unique values
+		elif 2 < df[feature].nunique() <= 16:
+			df = pd.concat(
+				[
+					df.drop([feature], axis=1),
+					pd.get_dummies(df[feature], drop_first=True, prefix=feature),
+				],
+				axis=1,
+			)
+	return df
 
 df = encode_categorical_features(df, valid_categorical_features)
 
@@ -78,7 +77,7 @@ df.columns = [col.replace("-", "_").replace(" ", "_") for col in df.columns]
 X = df.drop([target], axis=1)
 Y = df[target]
 Train_X, Test_X, Train_Y, Test_Y = train_test_split(
-    X, Y, train_size=0.8, test_size=0.2, random_state=100
+	X, Y, train_size=0.8, test_size=0.2, random_state=100
 )
 
 # Feature Scaling (Standardization)
@@ -97,137 +96,109 @@ pred_test = MLR.predict(Test_X_std)
 
 
 def prepare_input_data(
-    area,
-    mainroad,
-    guestroom,
-    basement,
-    hotwaterheating,
-    airconditioning,
-    prefarea,
-    additional_bedrooms,
-    bathrooms,
-    stories,
-    parking,
-    furnishingstatus,
+	area,
+	mainroad,
+	guestroom,
+	basement,
+	hotwaterheating,
+	airconditioning,
+	prefarea,
+	additional_bedrooms,
+	bathrooms,
+	stories,
+	parking,
+	furnishingstatus,
 ):
-    # Creates a dictionary for the input features
-    input_data = {
-        "area": [area],
-        "mainroad": True if mainroad == "Yes" else False,
-        "guestroom": True if guestroom == "Yes" else False,
-        "basement": True if basement == "Yes" else False,
-        "hotwaterheating": True if hotwaterheating == "Yes" else False,
-        "airconditioning": True if airconditioning == "Yes" else False,
-        "prefarea": True if prefarea == "Yes" else False,
-        "bedrooms_2": additional_bedrooms == 2,
-        "bedrooms_3": additional_bedrooms == 3,
-        "bedrooms_4": additional_bedrooms == 4,
-        "bedrooms_5": additional_bedrooms == 5,
-        "bedrooms_6": additional_bedrooms == 6,
-        "bathrooms_2": bathrooms == 2,
-        "bathrooms_3": bathrooms == 3,
-        "bathrooms_4": bathrooms == 4,
-        "stories_2": stories == 2,
-        "stories_3": stories == 3,
-        "stories_4": stories == 4,
-        "parking_1": parking == 1,
-        "parking_2": parking == 2,
-        "parking_3": parking == 3,
-        "furnishingstatus_semi_furnished": furnishingstatus == "semi_furnished",
-        "furnishingstatus_unfurnished": furnishingstatus == "unfurnished",
-    }
+	# Creates a dictionary for the input features
+	input_data = {
+		"area": [area],
+		"mainroad": True if mainroad == "Yes" else False,
+		"guestroom": True if guestroom == "Yes" else False,
+		"basement": True if basement == "Yes" else False,
+		"hotwaterheating": True if hotwaterheating == "Yes" else False,
+		"airconditioning": True if airconditioning == "Yes" else False,
+		"prefarea": True if prefarea == "Yes" else False,
+		"bedrooms_2": additional_bedrooms == 2,
+		"bedrooms_3": additional_bedrooms == 3,
+		"bedrooms_4": additional_bedrooms == 4,
+		"bedrooms_5": additional_bedrooms == 5,
+		"bedrooms_6": additional_bedrooms == 6,
+		"bathrooms_2": bathrooms == 2,
+		"bathrooms_3": bathrooms == 3,
+		"bathrooms_4": bathrooms == 4,
+		"stories_2": stories == 2,
+		"stories_3": stories == 3,
+		"stories_4": stories == 4,
+		"parking_1": parking == 1,
+		"parking_2": parking == 2,
+		"parking_3": parking == 3,
+		"furnishingstatus_semi_furnished": furnishingstatus == "semi_furnished",
+		"furnishingstatus_unfurnished": furnishingstatus == "unfurnished",
+	}
 
-    return pd.DataFrame(input_data)
+	return pd.DataFrame(input_data)
 
 # Note: Not removing this fxn because of the warning in predict.py file
 
 ### Final Endpoint ###
 # Predicts the price of a house based on the input features
 def house_price_prediction_model(
-    area=0,
-    mainroad=False,
-    guestroom=False,
-    basement=False,
-    hotwaterheating=False,
-    airconditioning=False,
-    prefarea=False,
-    bedrooms=0,
-    bathrooms=2,
-    stories=1,
-    parking=1,
-    furnishingstatus="semi_furnished",
+	area=0,
+	mainroad=False,
+	guestroom=False,
+	basement=False,
+	hotwaterheating=False,
+	airconditioning=False,
+	prefarea=False,
+	bedrooms=0,
+	bathrooms=2,
+	stories=1,
+	parking=1,
+	furnishingstatus="semi_furnished",
 ):
-    # Modifying the input data to match the model's input format
-    input_df = prepare_input_data(
-        area,
-        mainroad,
-        guestroom,
-        basement,
-        hotwaterheating,
-        airconditioning,
-        prefarea,
-        bedrooms,
-        bathrooms,
-        stories,
-        parking,
-        furnishingstatus,
-    )
+	# Modifying the input data to match the model's input format
+	input_df = prepare_input_data(
+		area,
+		mainroad,
+		guestroom,
+		basement,
+		hotwaterheating,
+		airconditioning,
+		prefarea,
+		bedrooms,
+		bathrooms,
+		stories,
+		parking,
+		furnishingstatus,
+	)
 
-    # Standardizes the input data
-    input_std = pd.DataFrame(std.transform(input_df), columns=input_df.columns)
+	# Standardizes the input data
+	input_std = pd.DataFrame(std.transform(input_df), columns=input_df.columns)
 
-    # Predicts the price
-    predicted_price = MLR.predict(input_std)
+	# Predicts the price
+	predicted_price = MLR.predict(input_std)
 
-    return round(predicted_price[0], 2)
-
-
-### Test ###
-def test_house_price_prediction():
-    # Sample inputs
-    area = 3000
-    mainroad = "Yes"
-    guestroom = "No"
-    basement = "Yes"
-    hotwaterheating = "No"
-    airconditioning = "Yes"
-    prefarea = "Yes"
-    bedrooms = 2
-    bathrooms = 3
-    stories = 2
-    parking = 2
-    furnishingstatus = "semi_furnished"
-
-    predicted_price = house_price_prediction_model(
-        area=area,
-        mainroad=mainroad,
-        guestroom=guestroom,
-        basement=basement,
-        hotwaterheating=hotwaterheating,
-        airconditioning=airconditioning,
-        prefarea=prefarea,
-        bedrooms=bedrooms,
-        bathrooms=bathrooms,
-        stories=stories,
-        parking=parking,
-        furnishingstatus=furnishingstatus,
-    )
-
-    print("Predicted House Price: Rs.", predicted_price)
+	return round(predicted_price[0], 2)
 
 
 def save_model():
-    # todo: Ask the user for the model name, and warn that the model will be overwritten
-    
-    with open("models/house_price/saved_models/model_01.pkl", "wb") as file:
-        pickle.dump(MLR, file)
+	# todo: Ask the user for the model name, and warn that the model will be overwritten
+	
+	with open("models/house_price/saved_models/model_01.pkl", "wb") as file:
+		pickle.dump(MLR, file)
 
 
 def save_scaler():    
-    with open("models/house_price/saved_models/scaler_01.pkl", "wb") as file:
-        pickle.dump(std, file)
-        
+	with open("models/house_price/saved_models/scaler_01.pkl", "wb") as file:
+		pickle.dump(std, file)
 
-if __name__ == "__main__":
-    save_model()
-    save_scaler()
+
+def model_evaluation():
+	me = ModelEvaluation(MLR, Train_X_std, Train_Y, Test_X_std, Test_Y)
+	me.evaluate()
+
+
+# if __name__ == "__main__":
+	# save_model()
+	# save_scaler()
+	# model_evaluation()
