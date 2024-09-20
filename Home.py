83b6e258d5746cff5fe9a5c4cfaedd9557519e30
yashasvini <ysmarina1234@gmail.com>
2024-09-21 01:23:00 +0530

    refactor: remove sidebar to simplify layout & updated app.py to Home.py

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Predictive Calc", page_icon="🧮")
+
+st.subheader("Project Description")
+st.write(
+	"This application provides a set of calculators to help you with financial decisions, including house price predictions and loan eligibility assessments."
+)
+
+st.subheader("Available Calculators:")
+st.write(
+	"- **House Price Prediction**: Estimate the price of a house based on various features."
+)
+st.write(
+	"- **Loan Eligibility**: Check your eligibility for different types of loans."
+)
