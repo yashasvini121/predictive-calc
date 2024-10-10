a3df141c6d797bd689a3068ac88482055509ad61
Yashasvini Sharma <100478608+yashasvini121@users.noreply.github.com>
2024-10-10 14:21:27 +0530

    Update model pickle path in model.py

--- models/Breast_Cancer_Detector/model.py
+++ models/Breast_Cancer_Detector/model.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 
-Loaded_model = pickle.load(open("saved models/model_Breast_cancer.pkl", 'rb'))
+Loaded_model = pickle.load(open(""models\Breast_Cancer_Detector\saved_models\Breast_cancer_model.pkl"", 'rb'))
 
 def cancer_type_prediction(input_data):
     input_data_as_numpy_array = np.asarray(input_data)
