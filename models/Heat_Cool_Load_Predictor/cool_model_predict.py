e2ef6c7662c22561020c2a0cb1bade3f7bc2657d
Yashasvini Sharma <100478608+yashasvini121@users.noreply.github.com>
2024-10-28 17:09:18 +0530

    Update get_prediction() fxn name

--- models/Heat_Cool_Load_Predictor/cool_model_predict.py
+++ models/Heat_Cool_Load_Predictor/cool_model_predict.py
@@ -1,4 +1,4 @@
 from models.Heat_Cool_Load_Predictor.model import cool_load_prediction
 
-def get_cool_prediction(relative_compactness, surface_area, wall_area, roof_area, overall_height, orientation, glazing_area, glazing_area_distribution):
+def get_prediction(relative_compactness, surface_area, wall_area, roof_area, overall_height, orientation, glazing_area, glazing_area_distribution):
     return cool_load_prediction(relative_compactness, surface_area, wall_area, roof_area, overall_height, orientation, glazing_area, glazing_area_distribution)
